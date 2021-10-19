ecca5184b38afda2be43e88904120acf11070074
karanysingh <56989043+karanysingh@users.noreply.github.com>
2021-10-20 05:14:00 +0530

    Add Heroku deployment files

--- firebase.py
+++ firebase.py
@@ -2,25 +2,20 @@ from firebase_admin import db
 import firebase_admin
 import json
 value = "-"
-ref = "-"
-def firebase_init():
-    databaseURL = "https://wireless-arduino-bot-default-rtdb.firebaseio.com/"
-    cred_object = firebase_admin.credentials.Certificate('./wireless-arduino-bot-firebase-adminsdk-jj79n-8b088d8efc.json')
-    default_app = firebase_admin.initialize_app(cred_object, {
-        'databaseURL':databaseURL
-        })
+databaseURL = "https://wireless-arduino-bot-default-rtdb.firebaseio.com/"
+cred_object = firebase_admin.credentials.Certificate('./wireless-arduino-bot-firebase-adminsdk-jj79n-8b088d8efc.json')
+default_app = firebase_admin.initialize_app(cred_object, {
+    'databaseURL':databaseURL
+    })
 
-    ref = firebase_admin.db.reference("/")
+ref = firebase_admin.db.reference("/")
 
-    value = ref.get("mode")[0]["mode"]
 
 def readState():
-    firebase_init()
+    value = ref.get("mode")[0]["mode"]
     return str(value)
 
 def modifyState(newVal):
-    firebase_init()
     ref.set({"mode":str(newVal)})
-    firebase_init()
     return readState()
     
